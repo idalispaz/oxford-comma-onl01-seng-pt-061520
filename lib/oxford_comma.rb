13c82150762069d9e84ab08c03b69fd4cced2e75
Idalis Paz <idalispaz@gmail.com>
2020-06-25 03:05:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,6 @@ fruit_array = ["kiwi","durian","starfruit","mangos","dragonfruit"]
 def oxford_comma(array)
   if array.length == 1 
     return array[0]
-  elsif array.length == 2 
-    return array.join(",")
   end
 end
 
