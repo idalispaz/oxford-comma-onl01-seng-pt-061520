5303f860c3abe5861ad30abfc6479b60e3e99e36
Idalis Paz <idalispaz@gmail.com>
2020-06-25 02:26:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  array = " Me, You, Them, Us"
+end 
+
+examples) 
+
+  ["a","b","c"].join
+  => "abc"
   
-end
+  "hippo,giragge,monkey,horse".split(",")
+   => ["hippo","giraffe","monkey","horse"]
+   
+   (1..10).to_a 
+   => [1,2,3,4,5,6,7,8,9,10]
+   
