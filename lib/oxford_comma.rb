8cc10e4805ba612527f551aecdac7daa737d0fc6
Idalis Paz <idalispaz@gmail.com>
2020-06-25 03:06:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ fruit_array = ["kiwi","durian","starfruit","mangos","dragonfruit"]
 def oxford_comma(array)
   if array.length == 1 
     return array[0]
-  elsif array
+  elsif array.length == 2 
+    return fruit_array.join
   end
 end
 
